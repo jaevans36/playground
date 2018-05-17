74c4d4457db14206ac06aa55bcbb4af5970a6970
jaevans36 <jaevans36@gmail.com>
2018-05-17 08:42:45 +0100

    Added scss and js watch tasks - tested and working fine

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var bs = require('browser-sync').create();
+var sass = require('gulp-sass');
+var browserify = require('gulp-browserify');
 
 gulp.task('start', function() {
   bs.init({
@@ -7,4 +9,31 @@ gulp.task('start', function() {
       baseDir: "./"
     }
   });
+
+  // add browserSync.reload to the tasks array to make
+  // all browsers reload after tasks are complete.
+  gulp.watch("app/js/*.js", ['js-watch']);
+});
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(bs.reload({stream: true})); // prompts reload after compilation
+});
+
+gulp.task('js', function() {
+  return gulp.src('app/js/*js')
+    .pipe(browserify())
+    .pipe(gulp.dest('dist/js'));
+})
+
+gulp.task('js-watch', ['js'], function(done) {
+  bs.reload();
+  done();
+});
+
+gulp.task('watch', ['start'], function() {
+  gulp.watch("app/scss/*.scss", ['sass']);
+  gulp.watch("*.html").on('change', bs.reload);
 });
