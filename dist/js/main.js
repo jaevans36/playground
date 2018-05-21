ba2c84afc9acc0e0334ee9fe66c4d686dd9d14d4
Jay <jaevans36@gmail.com>
2018-05-21 18:04:36 +0100

    Added prototype and constructor combined and dynamic prototype patterns to examples

--- dist/js/main.js
+++ dist/js/main.js
@@ -56,6 +56,7 @@ function Album(name, numOfTracks, releaseMonth) {
 
 var albumOne = new Album('Something something', 12, 'August');
 // albumOne.sayAlbumName(); // 'Something something'
+
 // Factory Pattern
 // Although this solves the problem of creating multiple similar objects, this pattern
 // doesn't address the issue of object identification (What type of object an object is).
@@ -144,6 +145,37 @@ Object.defineProperties(moreBooks, {
 // moreBooks.year = 2008;
 
 // console.log(moreBooks);
+// Combining Constructor and Prototype Pattern for custom types
+// This is useful because using the prototype constructor on its own,
+// means that values are shared between all objects created with this
+// prototype. With this approach, each instance ends up with its own
+// copy of instance properties.
+
+function PersonConst(name, age, job) {
+  this.name = name;
+  this.age = age;
+  this.job = job;
+  this.friends = ['Jim', 'Dwight'];
+}
+
+PersonConst.prototype = {
+  constructor: Person,
+  sayName: function sayName() {
+    alert(this.name);
+  }
+};
+
+var person1 = new PersonConst('Nicholas', 29, 'Software Engineer');
+var person2 = new PersonConst('Greg', 27, 'Doctor');
+
+// console.log(person1);
+
+person1.friends.push('Van');
+
+// alert(person1.friends); // Jim, Dwight, Van
+// alert(person2.friends); // Jim, Dwight
+// alert(person1.friends === person2.friends); //false
+// alert(person1.sayName === person2.sayName); //true
 // Prototype pattern
 //
 // The benefi t of using the prototype is that all of its properties and methods are shared among object instances
