c50990fe8013f09f17aa683c40ea50a25dc91a81
jaevans36 <jaevans36@gmail.com>
2018-06-06 15:01:19 +0100

    Change api example call to be ignored and added new module for team assignment for world cup draw

---
+++ app/js/examples/teamAssign.js
@@ -0,0 +1,76 @@
+let teamsArray = [];
+let teamsObjList = [];
+
+// Get all the HTML content from a URL as a string
+let getURL = function (url, success, error) {
+  if (!window.XMLHttpRequest) return;
+
+  var request = new XMLHttpRequest();
+
+  request.onreadystatechange = function() {
+    if (request.readyState == 4) {
+      if(request.status !== 200) {
+        if (error && typeof error === 'function') {
+          error(request.responseText, request);
+        }
+        return;
+      }
+
+      if (success && typeof success === 'function') {
+        success(request.responseText, request);
+      }
+    }
+  }
+
+  request.open('GET', url);
+  request.send();
+}
+
+// using getURL function to scrape teams from Worldcup site.
+getURL(
+  'http://www.fifa.com/worldcup/teams/index.html',
+  function(data) {
+    console.log('success:');
+
+    var div = document.createElement(div);
+    div.innerHTML = data;
+
+    var element = div.querySelectorAll('.team');
+    // Grab element on the page to spit the list in to
+    // var teamList = document.querySelector( '#teams' );
+
+    console.log(element);
+
+    if (!element) return;
+
+    for (let i = 0; i < element.length; i++) {
+      teamsObjList[i] = {
+        teamName: element[i].innerText,
+        assigned: {
+          isAssigned: false,
+          assignTo: ''
+        },
+        flagImg: element[i].childNodes[0].currentSrc
+      };
+      // Add each team found to the page
+      // teamList.innerHTML += element[i].innerHTML;
+    }
+  },
+  function(data) {
+    console.log('error:');
+  }
+);
+
+// for (let i = 0; i < teamsArray.length; i++) {
+//   teamsObjList[i] = {
+//     teamName: teamsArray[i],
+//     assigned: false,
+//     assignedTo: ''
+//   };
+// }
+
+console.log(teamsObjList);
+
+var assignTeam = function (name, obj) {
+  var objLength = obj.length;
+}
